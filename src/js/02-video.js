8b1e3ba190aa251b08216dbbf43e24674b1920f5
Serhiy Slipchuk <slipchuks@ukr.net>
2023-03-11 13:58:28 +0200

    task 02 without throttle

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,8 +1,28 @@
 import Player from '@vimeo/player';
 
-const LOCALSTOREGE_KEY = 'videoplayer-current-time';
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-console.log(player);
+player.on('timeupdate', onPlay);
 
+const currentTime = localStorage.getItem(LOCALSTORAGE_KEY);
+
+player.setCurrentTime(currentTime).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
+
+function onPlay(data) {
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(data.seconds));
+    console.log(data.seconds);
+}
