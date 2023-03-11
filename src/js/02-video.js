7144b001466f9a36922c4f9d7f761505df4d81b9
Serhiy Slipchuk <slipchuks@ukr.net>
2023-03-11 14:20:43 +0200

    task 02 complete

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,11 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const LOCALSTORAGE_KEY = 'videoplayer-current-time';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('timeupdate', onPlay);
+player.on('timeupdate', throttle(onPlay, 1000));
 
 const currentTime = localStorage.getItem(LOCALSTORAGE_KEY);
 
